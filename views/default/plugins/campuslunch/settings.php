15e740a3c651f497b2087d3b409457862b31a713
Brad Smith <bradmsmith@live.com>
2013-07-19 23:53:30 -0700

    Plugin settings

---
+++ views/default/plugins/campuslunch/settings.php
@@ -0,0 +1,5 @@
+<p>
+  <?php echo elgg_echo('campuslunch:settings:bloggers'); ?>
+ 
+  <br/><textarea name="params[bloggers]" cols="50" placeholder="Comma-seperated usernames of bloggers"></textarea>
+</p>
