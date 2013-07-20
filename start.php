251b205a6465d28ad488b4309dd74ac24d8c2b70
Brad Smith <bradmsmith@live.com>
2013-07-20 16:06:33 -0700

    Page handler for lunch form

--- start.php
+++ start.php
@@ -1,5 +1,16 @@
 <?php
-
+	
+	// Action for saving lunch
 	elgg_register_action("lunch/save", elgg_get_plugins_path() . "lunch/actions/lunch/save.php");
 
+	// Handler for serving lunch form
+	elgg_register_page_handler('lunch', 'lunch_page_handler');
+
+	function lunch_page_handler($segments) {
+	    if ($segments[0] == 'add') {
+	        include elgg_get_plugins_path() . 'lunch/pages/lunch/add.php';
+	        return true;
+	    }
+	    return false;
+	}
 ?>
