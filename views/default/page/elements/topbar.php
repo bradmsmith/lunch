1052a9809696b88fa0f8f169054cca4a82923c3b
Brad Smith <bradmsmith@live.com>
2013-07-20 23:14:01 -0700

    Formatting

--- views/default/page/elements/topbar.php
+++ views/default/page/elements/topbar.php
@@ -1,2 +1,1 @@
-
 <strong><a href="/profile"><img src="<?php echo get_loggedin_user()->getIconURL('tiny');?>" /><?php echo get_loggedin_user()->name;  ?></a></strong><br/><a href="/settings">Settings</a> | <a href="/action/logout">Sign Out</a>
