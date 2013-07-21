c090eb2c8c3e8cd7a0f5ab1644102a54ea5690b0
Brad Smith <bradmsmith@live.com>
2013-07-20 19:41:41 -0700

    Formating

--- start.php
+++ start.php
@@ -5,6 +5,9 @@
 
 	// Handler for serving lunch form
 	elgg_register_page_handler('lunch', 'lunch_page_handler');
+	
+	// Menus
+	elgg_register_plugin_hook_handler('register', 'menu:owner_block', 'lunch_owner_block_menu');
 		
 	function lunch_page_handler($segments) {
 	    switch ($segments[0]) {
@@ -20,4 +23,19 @@
 
 	    return true;
 	}
+	
+	/**
+	 * add menu item to owner block
+	 */
+	function lunch_owner_block_menu($hook, $entity_type, $returnvalue, $params){
+		$group = elgg_extract("entity", $params);
+		if (elgg_instanceof($group, 'group') && $group->event_manager_enable != "no") {
+			$url = '/lunch/all/' . $group->getGUID();
+			$item = new ElggMenuItem('lunch', elgg_echo('lunch:menu:lunches'), $url);
+			$returnvalue[] = $item;
+		}
+		
+		return $returnvalue;
+	}
+	
 ?>
