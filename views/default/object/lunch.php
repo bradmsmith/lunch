cbda133fe06b9de459d608f024753e91ac9c9a9b
bradmsmith <bradmsmith@live.com>
2013-07-22 22:05:27 -0700

    Lunch object - show group

--- views/default/object/lunch.php
+++ views/default/object/lunch.php
@@ -1,5 +1,9 @@
 <?php 
+$container = $vars['entity']->getContainerEntity();
+
 echo elgg_view_title($vars['entity']->title);
 echo elgg_view('output/longtext', array('value' => $vars['entity']->description));
 echo elgg_view('output/tags', array('tags' => $vars['entity']->tags)); 
+echo elgg_view('output/longtext', array('value' => $container->name));
+
 ?>
