5eb13d7816ee1273b7a6029a78af32fefe82f9c7
Brad Smith <bradmsmith@live.com>
2013-08-27 11:14:58 -0700

    Update custom index page

--- pages/index.php
+++ pages/index.php
@@ -1,1 +1,9 @@
-<h1>test</h1>
+<?php
+
+$title = "Welcome";
+$content = elgg_view('page/index');
+$body = elgg_view_layout('one_sidebar', array('content' => $content));
+
+echo elgg_view_page($title, $body);
+
+?>
---
+++ views/default/page/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$login_box = elgg_view('core/account/login_box', array('module' => 'walledgarden-login'));
+echo $login_box;
+?>
+
+
+<div class="desc">
+<h1>Campus Lunch.</h1>
+<p>It seems like such a simple idea. We’ll invite people for free food and preach them the gospel of Jesus Christ. Once a week we can talk about the Bible during lunch on your campus by having a Christian Club. And yet, this simple idea will involve your entire life.</p>
+<p>Representing the gospel is not just something you can do once a week, it is something you are.  It is a 24/7 lifestyle.</p>
+<p> This site will equip you to evangelize at your high school.  There are so many who need to know Jesus but won’t read the Bible or go to church.  Let’s take Jesus to them.</p>
+<a id="signup_button" href="/register">Sign Up</a>
+</div>
+
+
