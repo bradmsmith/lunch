4b8687617e728c36e3171aacc33a478ecf8fe0d4
Brad Smith <bradmsmith@live.com>
2013-08-26 23:30:58 -0700

    Make map larger

--- pages/map/index.php
+++ pages/map/index.php
@@ -1,56 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<title>Google Maps Multiple Markers</title>
-<script src="http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyASijUpE9UZ7c_bpzpN1rW4-FEeb5wiYac"
-type="text/javascript"></script>
-</head>
-<body>
-
-<h1>test</h1>
 <?php
-    // Load array of schools and geocodes from group objects
-    $schools = array();
-    $groups = elgg_get_entities(array('types' => 'group', 'limit' => false));
     
-    foreach($groups as $group) {
-        $schools[] = '[\'' . $group->name . '\', ' . $group->geocode[0] .', ' . $group->geocode[1] . '],';
-    }
-    
-    var_dump($schools);
-    
-    ?>
-
-
-<div id="map" style="width: 500px; height: 400px;"></div>
-
-<script type="text/javascript">
-var schools = [<?php foreach ($schools as $school) { echo $school; }; ?>];
-
-var map = new google.maps.Map(document.getElementById('map'), {
-                              zoom: 10,
-                              center: new google.maps.LatLng(-33.92, 151.25),
-                              mapTypeId: google.maps.MapTypeId.ROADMAP
-                              });
-
-var infowindow = new google.maps.InfoWindow();
-
-var marker, i;
-
-for (i = 0; i < schools.length; i++) {
-    marker = new google.maps.Marker({
-                                    position: new google.maps.LatLng(schools[i][1], schools[i][2]),
-                                    map: map
-                                    });
-    
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                                                    return function() {
-                                                    infowindow.setContent(schools[i][0]);
-                                                    infowindow.open(map, marker);
-                                                    }
-                                                    })(marker, i));
+// Load array of schools and geocodes from group objects
+$schools = "";
+$groups = elgg_get_entities(array('types' => 'group', 'limit' => false));
+foreach($groups as $group) {
+        $schools .= '[\'' . $group->name . '\', ' . $group->geocode[0] .', ' . $group->geocode[1] . '],';
 }
-</script>
-</body>
-</html>
+
+
+$content = <<<CONTENT
+    
+    <script src="http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyASijUpE9UZ7c_bpzpN1rW4-FEeb5wiYac"
+    type="text/javascript"></script>
+    
+    <div id="map" style="width: 100%; height: 500px;"></div>
+    
+    <script type="text/javascript">
+    
+    var marker, i;
+    var schools = [{$schools}];
+    var bounds = new google.maps.LatLngBounds();
+    var map = new google.maps.Map(document.getElementById('map'), {
+                                  zoom: 10,
+                                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                                  });
+    var infowindow = new google.maps.InfoWindow();
+    for (i = 0; i < schools.length; i++) {
+        marker = new google.maps.Marker({
+                                        position: new google.maps.LatLng(schools[i][1], schools[i][2]),
+                                        map: map
+                                        });
+        
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                                                        return function() {
+                                                        infowindow.setContent(schools[i][0]);
+                                                        infowindow.open(map, marker);
+                                                        }})(marker, i));
+        bounds.extend(marker.position);
+        map.fitBounds(bounds);
+    }
+    </script>
+
+CONTENT;
+
+    
+$body = elgg_view_layout('one_column', $content);
+
+echo elgg_view_page($title, $body);
+?>
