4f9bff8b0b51a2568141d36d78b12b8ff11709e3
Brad Smith <bradmsmith@live.com>
2013-08-26 22:56:41 -0700

    Add school markers to map

--- pages/map/index.php
+++ pages/map/index.php
@@ -7,16 +7,26 @@
 type="text/javascript"></script>
 </head>
 <body>
+
+<h1>test</h1>
+<?php
+    // Load array of schools and geocodes from group objects
+    $schools = array();
+    $groups = elgg_get_entities(array('types' => 'group', 'limit' => false));
+    
+    foreach($groups as $group) {
+        $schools[] = '[\'' . $group->name . '\', ' . $group->geocode[0] .', ' . $group->geocode[1] . '],';
+    }
+    
+    var_dump($schools);
+    
+    ?>
+
+
 <div id="map" style="width: 500px; height: 400px;"></div>
 
 <script type="text/javascript">
-var schools = [
-['Bondi Beach', -33.890542, 151.274856, 4],
-['Coogee Beach', -33.923036, 151.259052, 5],
-['Cronulla Beach', -34.028249, 151.157507, 3],
-['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-['Maroubra Beach', -33.950198, 151.259302, 1]
-];
+var schools = [<?php foreach ($schools as $school) { echo $school; }; ?>];
 
 var map = new google.maps.Map(document.getElementById('map'), {
                               zoom: 10,
