45b19c5caf25423cf7e8ef82fea80adc30c03487
Brad Smith <bradmsmith@live.com>
2013-08-31 18:33:07 -0700

    Update topic page

--- pages/topic/all.php
+++ pages/topic/all.php
@@ -3,23 +3,31 @@
 $owner = elgg_get_page_owner_entity();
 
 $title = elgg_echo('lunch:topic:all');
+elgg_view_title($title, array('class' => 'elgg-heading-main'));
 
-$content = elgg_view_title($title, array('class' => 'elgg-heading-main'));
+// Logged in
+if (elgg_is_logged_in()){
 
-// Display add button for moderators
-if (lunchmoderator(false)) {
-	// This needs to be updated using elgg_register_menu/title
-	$content .= '<ul class="elgg-menu elgg-menu-title elgg-menu-hz elgg-menu-title-default"><li class="elgg-menu-item-add"><a href="/topic/add/" class="elgg-button elgg-button-action">Add topic</a></li></ul>';
+	$content = "";
+	// Display add button for moderators
+	if (lunchmoderator(false)) {
+		// This needs to be updated using elgg_register_menu/title
+		$content .= '<ul class="elgg-menu elgg-menu-title elgg-menu-hz elgg-menu-title-default"><li class="elgg-menu-item-add"><a href="/topic/add/" class="elgg-button elgg-button-action">Add topic</a></li></ul>';
+	}
+
+	$content .= elgg_list_entities(array(
+	    'type' => 'object',
+	    'subtype' => 'topic',
+		'container_guid' => $owner->guid,
+		'full_view' => FALSE,
+	));
+	
+	$sidebar = "Sidebar content";
+} else {
+	// Logged out
+	$content = elgg_view('page/signup/index');
+	$sidebar = "Sidebar content";
 }
-
-$content .= elgg_list_entities(array(
-    'type' => 'object',
-    'subtype' => 'topic',
-	'container_guid' => $owner->guid,
-	'full_view' => FALSE,
-));
-
-$sidebar = "Sidebar content";
 
 $body = elgg_view_layout('one_sidebar', array(
 	'content' => $content,
