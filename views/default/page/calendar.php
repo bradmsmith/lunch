ea7d97e988c62116e4f01dc971aee3d3009d293e
Brad Smith <bradmsmith@live.com>
2013-09-01 09:46:41 -0700

    Move page views to lunch_theme

---
+++ views/default/page/calendar.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Display calendar
+ */
+
+elgg_load_js('fullcalendar');
+elgg_load_css('fullcalendar');
+
+?>
+
+<script>
+$(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+		events: {
+			url: '/services/api/rest/json/?method=lunch',
+			color: '#00bff3',
+			textColor: 'white',
+		},
+		buttonText: {
+			today: 'Today'
+		},
+		weekMode: 'variable',
+	});
+	
+});
+</script>
+
+<div id="calendar"></div>
+
+
+ 
