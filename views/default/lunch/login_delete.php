5f71d12948e11b1ff33720d7843f4a06f72520e4
Brad Smith <bradmsmith@live.com>
2013-09-07 09:55:47 -0700

    Move view to theme

--- views/default/lunch/login_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-// allow plugins to override the login page
-if (elgg_trigger_plugin_hook('login', 'lunch', null, FALSE) != FALSE) {
-	echo "login yes";
-	exit;
-} else {
-	echo "login no";
-}
