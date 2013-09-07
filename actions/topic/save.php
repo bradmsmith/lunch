c98366aec4965ae9b9d1ad91fb25a0468c3e90ac
Brad Smith <bradmsmith@live.com>
2013-09-07 09:55:37 -0700

    Add image to topic

--- actions/topic/save.php
+++ actions/topic/save.php
@@ -1,22 +1,44 @@
 <?php
+/**
+ * Save new topic
+ * asdfarray (
+ * 'name' => 'Whatsyourstory.jpg',
+ * 'type' => 'image/jpeg',
+ * 'tmp_name' => '/Applications/XAMPP/xamppfiles/temp/phpfBAemO',
+ * 'error' => 0,
+ * 'size' => 16065,
+ * )
+ * @todo image file and type checks
+ */
+
 // get the form inputs
 $title = get_input('title');
-$body = get_input('body');
-$tags = string_to_tag_array(get_input('tags'));
+$summary = get_input('summary');
+$description = get_input('description');
+$image = $_FILES['image'];
  
 // create a new topic object
 $object = new ElggObject();
 $object->subtype = "topic";
 $object->title = $title;
-$object->description = $body;
-$object->tags = $tags;
- 
-// for now make all topic posts public
-$object->access_id = ACCESS_PUBLIC;
+$object->summary = $summary;
+$object->description = $description; // . var_export($image, true);
+$object->access_id = ACCESS_PUBLIC; // for now make all topic posts public
 $object->owner_guid = elgg_get_logged_in_user_guid();
-
-// save to database and get id of the new lunch
 $object_guid = $object->save();
+
+// create a new topic image
+$file = new ElggFile();
+$filename = 'topic/' . $object->guid . '.jpg';
+$file->container_guid = $object->guid;
+$file->owner_guid = $object->guid;
+$file->setFilename($filename);
+$file->setMimeType($_FILES['image']['type']);
+$file->originalfilename = $FILES['image']['name'];
+$file->open("write");
+$file->write(get_uploaded_file('image'));
+$file->close();
+$file_guid = $file->save();
 
 // if the lunch was saved, we want to display the new post
 // otherwise, we want to register an error and forward back to the form
