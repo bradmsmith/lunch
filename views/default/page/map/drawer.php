c6150f7e55f64e64a089295ebf30d0a7fab0f9a9
Brad Smith <bradmsmith@live.com>
2013-09-07 18:42:33 -0700

    Update maps view

---
+++ views/default/page/map/drawer.php
@@ -0,0 +1,10 @@
+<?
+/**
+ * Drawer
+ */
+?>
+
+<script src="http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyASijUpE9UZ7c_bpzpN1rW4-FEeb5wiYac"
+type="text/javascript"></script>
+
+<div id="map" style="width: 100%; height: 400px; width: 960px; margin: auto"></div><br/>
