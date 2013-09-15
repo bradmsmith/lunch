0e26835cc0ef1cf0d7fe55e21b3e064fb8d42393
Brad Smith <bradmsmith@live.com>
2013-09-14 18:00:53 -0700

    Add drawer to how-to page

--- lunch/pages/howto/index.php
+++ lunch/pages/howto/index.php
@@ -2,12 +2,16 @@
 
 $title = "How To";
 $content = elgg_view('page/howto/index');
+$drawer = elgg_view('page/howto/drawer');
+$vars = array('drawer' => $drawer);
+
 $sidebar = "sidebar";
 
 $body = elgg_view_layout('one_column', array(
 	'content' => $content,
+	'drawer' => $drawer,
 ));
  
-echo elgg_view_page($title, $body);
+echo elgg_view_page($title, $body, 'default', $vars);
 
 ?>
