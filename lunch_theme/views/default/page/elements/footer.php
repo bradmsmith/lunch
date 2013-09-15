c5ec7dc6565314d65a1ff993697cce87ccb04d1e
Brad Smith <bradmsmith@live.com>
2013-09-14 18:01:17 -0700

    Add logo to footer

--- lunch_theme/views/default/page/elements/footer.php
+++ lunch_theme/views/default/page/elements/footer.php
@@ -10,6 +10,8 @@
 
 echo elgg_view_menu('footer', array('sort_by' => 'priority', 'class' => 'elgg-menu-hz'));
 
+echo '<img src="/mod/lunch_theme/graphics/campuslunches.png" width="125" style="padding-top: 10px"/>';
+
 echo '<div class="mts clearfloat float-alt">';
 echo '<p>Copyright &copy; 2013</p>';
 echo '</div>';
