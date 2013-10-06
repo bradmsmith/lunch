9bc2f9a27a1b686d9130e8b2cf3eed9275280025
Brad Smith <bradmsmith@live.com>
2013-10-05 21:39:28 -0700

    Format forgot password
    Close #20

--- views/default/forms/login.php
+++ views/default/forms/login.php
@@ -1,5 +1,7 @@
-<?php echo elgg_view('input/text', array('name' => 'username', 'class' => 'elgg-autofocus', 'placeholder' => elgg_echo('loginusername'))); ?>
-<?php echo elgg_view('input/password', array('name' => 'password', 'placeholder' => elgg_echo('password'))); ?>
+<a href="/forgotpassword" style="float: right; width: 40px; margin-left: 15px;">Forgot Pass?</a>
+<?php echo elgg_view('input/text', array('name' => 'username', 'class' => 'elgg-autofocus', 'placeholder' => elgg_echo('loginusername'), 'size' => '15')); ?>
+<?php echo elgg_view('input/password', array('name' => 'password', 'placeholder' => elgg_echo('password'), 'size' => '15')); ?>
 <?php echo elgg_view('input/submit', array('value' => elgg_echo('login'), 'id' => 'signup_button')); ?>
 <?php echo elgg_view('input/hidden', array('name' => 'persistent', 'value' => 'true')); ?>
-<br/><a href="/forgotpassword" style="float: right; padding-right: 70px;">Forgot password?</a>
+
+
